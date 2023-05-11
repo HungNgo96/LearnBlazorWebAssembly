807cf8747b25f7933eb0ca064ef8095b0a7ebffd
HungNT145 <HungNT145@fpt.com.vn>
2023-05-11 22:52:59 +0700

    2

---
+++ BlazorWebApp/Shared/SurveyPrompt.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebApp.Shared
+{
+    public partial class SurveyPrompt
+    {
+        // Demonstrates how a parent component can supply parameters
+        [Parameter]
+        public string? Title { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object> ImageAttr1 { get; set; }
+
+        [CascadingParameter(Name = "CasName")]
+        public string CascadingValueDemo { get; set; }
+    }
+}
