2dda6181a9f77cd70229dbcf93e23a6ce0b35c09
HungNgo <hung.ngothanh96@gmail.com>
2023-05-22 17:38:21 +0700

    call C# to Javascipt, call JS to C#

--- BlazorWebApp/Pages/CallJavaScriptInDotNet.razor.cs
+++ BlazorWebApp/Pages/CallJavaScriptInDotNet.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorWebApp.Model;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
 namespace BlazorWebApp.Pages
@@ -10,24 +11,50 @@ namespace BlazorWebApp.Pages
 
         private IJSObjectReference _jsModule;
 
+        private string _registrationResult;
+        private string _detailsMessage;
+        private ElementReference _elRef;
+        private EmailDetails _emailDetails = new EmailDetails();
+
         protected override async Task OnInitializedAsync()
         {
 
         }
-
-        public async Task ShowAlertWindow()
-        {
-            await _jsModule.InvokeVoidAsync("showAlert", "JS function called from .NET");
-            //await JSRuntime.InvokeVoidAsync("showAlert", "JS function called from .NET");
-        }
-
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
             {
                 Console.WriteLine("OnAfterRenderAsync firstRender");
                 _jsModule = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./scripts/jsExamples.js");
+                await FocusAndStyleElement();//call update elmRef after render
+                //await ThrowError();
             }
         }
+        public async Task ShowAlertWindow()
+        {
+            //await _jsModule.InvokeVoidAsync("showAlert", "JS function called from .NET");
+            await _jsModule.InvokeVoidAsync("showAlertWithParameter", new { Name = "John", Age = 35 });
+            //await JSRuntime.InvokeVoidAsync("showAlert", "JS function called from .NET");
+        }
+        private async Task RegisterEmail() =>
+        _registrationResult = await _jsModule.InvokeAsync<string>("emailRegistration", "Please provide your email");
+
+        private async Task ExtractEmailInfo()
+        {
+            var emailDetails = await _jsModule.InvokeAsync<EmailDetails>("splitEmailDetails", "Please provide your email");
+            if (emailDetails != null)
+                _detailsMessage = $"Name: {emailDetails.Name}, Server: {emailDetails.Server}, Domain: {emailDetails.Domain}";
+            else
+                _detailsMessage = "Email is not provided.";
+        }
+
+        private async Task FocusAndStyleElement()
+      => await _jsModule.InvokeVoidAsync("focusAndStyleElement", _elRef);
+
+        private async Task FocusAndStyleInputComonent()
+    => await _jsModule.InvokeVoidAsync("focusAndStyleInputComponent", "dummyInputComponent");
+
+        private async Task ThrowError()
+    => await _jsModule.InvokeVoidAsync("throwError");
     }
 }
---
+++ BlazorWebApp/wwwroot/scripts/jsExamples2.js
@@ -0,0 +1,56 @@
+var jsFunctions = {};
+let projectName = "BlazorWebApp";
+jsFunctions.calculateSquareRoot = function () {
+    const number = prompt("Enter your number");
+
+    DotNet.invokeMethodAsync(projectName, "CalculateSquareRoot", parseInt(number))
+        .then(result => {
+            let el = document.getElementById("string-result");
+            el.innerHTML = result;
+        });
+}
+
+jsFunctions.calculateSquareRootIdentifierName = function () {
+    const number = prompt("Enter your number");
+    let IdentifierName = CalculateSquareRootWithJustResult;
+    DotNet.invokeMethodAsync(projectName, IdentifierName, parseInt(number))
+        .then(result => {
+            let el = document.getElementById("string-result");
+            el.innerHTML = result;
+        });
+}
+
+jsFunctions.showMouseCoordinates = function (dotNetObjRef) {
+    dotNetObjRef.invokeMethodAsync("ShowCoordinates",
+        {
+            x: window.event.screenX,
+            y: window.event.screenY
+        }
+    );
+}
+
+jsFunctions.registerMouseCoordinatesHandler = function (dotNetObjRef) {
+    function mouseCoordinatesHandler() {
+        dotNetObjRef.invokeMethodAsync("ShowCoordinates",
+            {
+                x: window.event.screenX,
+                y: window.event.screenY
+            }
+        );
+    };
+
+    mouseCoordinatesHandler();
+
+    document.getElementById("coordinates").onmousemove = mouseCoordinatesHandler;
+}
+
+jsFunctions.registerOnlineStatusHandler = function (dotNetObjRef) {
+    function onlineStatusHandler() {
+        dotNetObjRef.invokeMethodAsync("SetOnlineStatusColor", navigator.onLine);
+    };
+
+    onlineStatusHandler();
+
+    window.addEventListener("online", onlineStatusHandler);
+    window.addEventListener("offline", onlineStatusHandler);
+}
